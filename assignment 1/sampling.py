acca7f7e79a8f3b1f71ee3fef9669c9a2cdecf2c
Anshm1234 <142911652+Anshm1234@users.noreply.github.com>
2026-01-17 16:40:19 +0530

    Update sampling.py

--- assignment 1/sampling.py
+++ assignment 1/sampling.py
@@ -10,16 +10,14 @@ data = pd.read_csv("Creditcard_data.csv")
 X = data.drop("Class", axis=1)
 y = data["Class"]
 
-# Scale
+
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# Split
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42, stratify=y
 )
 
-# Sampling methods dictionary
 sampling_methods = {
     "Sampling1": RandomUnderSampler(random_state=42),
     "Sampling2": RandomOverSampler(random_state=42),
@@ -27,3 +25,4 @@ sampling_methods = {
     "Sampling4": NearMiss(),
     "Sampling5": SMOTETomek(random_state=42)
 }
+
